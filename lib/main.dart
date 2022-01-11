409302581d920297c23ccab92fbbce9f86f2121b
Silent Cat <ngocphuocnguyen312@gmail.com>
2022-01-12 00:25:08 +0700

    finished

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:counter_app/bloc_observer.dart';
+import 'package:counter_app/counter_app_bloc_observer.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
